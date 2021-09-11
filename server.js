2fd7a4fe72d5620793f8e7600b692eaec4c22175
BritosMarco <marco.britocn@gmail.com>
2021-09-10 22:22:17 -0300

    Ultima_Aula

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const connectToDb = require("./src/database/database");
+
+connectToDb();
 
 const app = express();
 const port = 3000;
 
-app.listen(port, () => console.log(`Servidor Rodando em localhost:${port}`));
+app.use(express.json());
+
+app.listen(port, () =>
+    console.info(`Servidor rodando em http://localhost:${port}`)
+);
